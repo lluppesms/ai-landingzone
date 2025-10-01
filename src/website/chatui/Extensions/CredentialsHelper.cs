725753a349a8a6e30e7b4d4cac29e71b84ed37a5
Lab Luppes <labs@luppes.com>
2025-10-01 09:47:57 -0500

    Add debugging to chat ui

---
+++ src/website/chatui/Extensions/CredentialsHelper.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Azure.Identity;
+
+namespace chatui.Extensions;
+
+public static class CredentialsHelper
+{
+    public static DefaultAzureCredential GetCredentials(IConfiguration configuration)
+    {
+        return GetCredentials(configuration["VisualStudioTenantId"], configuration["UserAssignedManagedIdentityClientId"]);
+    }
+
+    public static DefaultAzureCredential GetCredentials(ChatApiOptions configuration)
+    {
+        return GetCredentials(configuration.VisualStudioTenantId, configuration.UserAssignedManagedIdentityClientId);
+    }
+
+    public static DefaultAzureCredential GetCredentials(string visualStudioTenantId, string userAssignedManagedIdentityClientId)
+    {
+        if (!string.IsNullOrEmpty(visualStudioTenantId))
+        {
+            var azureCredential = new DefaultAzureCredential(new DefaultAzureCredentialOptions
+            {
+                VisualStudioTenantId = visualStudioTenantId,
+                Diagnostics = { IsLoggingContentEnabled = true }
+            });
+            return azureCredential;
+        }
+        else
+        {
+            if (!string.IsNullOrEmpty(userAssignedManagedIdentityClientId))
+            {
+                var azureCredential = new DefaultAzureCredential(new DefaultAzureCredentialOptions
+                {
+                    ManagedIdentityClientId = userAssignedManagedIdentityClientId,
+                    Diagnostics = { IsLoggingContentEnabled = true }
+                });
+                return azureCredential;
+            }
+            else
+            {
+                var azureCredential = new DefaultAzureCredential(new DefaultAzureCredentialOptions
+                {
+                    Diagnostics = { IsLoggingContentEnabled = true }
+                });
+                return azureCredential;
+            }
+        }
+    }
+}
